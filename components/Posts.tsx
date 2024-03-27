6b4fc5a28d9aeef047282277e7ec423d53283e43
sergio esteban torres <myplancash@gmail.com>
2024-03-27 11:00:14 -0500

    explicitly specify the type of the initial state for posts in the useState hook. Since posts is an array of BlogPost

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import { getSortedPostsData } from "@/lib/posts";
 import ListItem from "./ListItem";
+import { BlogPost } from "@/types";
 
 export default function Posts() {
-    const posts = getSortedPostsData();
+    const [posts, setPosts] = useState<BlogPost[]>([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const sortedPosts = await getSortedPostsData();
+            setPosts(sortedPosts);
+        }
+        fetchPosts();
+    }, []);
 
     return (
         <section className="mt-12 mb-12 mx-auto max-w-2xl w-full animate-fadeIn transition duration-200 ease-out">
--- lib/posts.tsx
+++ lib/posts.tsx
@@ -7,34 +7,32 @@ import { BlogPost } from '@/types'
 
 const postsDirectory = path.join(process.cwd(), 'blogposts')
 
-export function getSortedPostsData() {
-    // Get file names under /posts
+export async function getSortedPostsData() {
     const fileNames = fs.readdirSync(postsDirectory);
-    const allPostsData = fileNames.map((fileName) => {
-        // Remove ".md" from file name to get id
+    const allPostsData = await Promise.all(fileNames.map(async (fileName) => {
         const id = fileName.replace(/\.md$/, '');
-
-        // Read markdown file as string
         const fullPath = path.join(postsDirectory, fileName);
         const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-        // Use gray-matter to parse the post metadata section
         const matterResult = matter(fileContents);
+        
+        // Fetch contentHtml asynchronously
+        const processedContent = await remark().use(html).process(matterResult.content);
+        const contentHtml = processedContent.toString();
 
         const blogPost: BlogPost = {
             id,
             title: matterResult.data.title,
             author: matterResult.data.author,
             date: matterResult.data.date,
-            
-        }
+            contentHtml,
+        };
 
-        // Combine the data with the id
-        return blogPost
-    });
-    // Sort posts by date
-    return allPostsData.sort((a, b) => a.date < b.date ? 1 : -1);
+        return blogPost;
+    }));
+
+    return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
+
 
 export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`);
