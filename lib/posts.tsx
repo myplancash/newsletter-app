4e7192c65e58afb946526e228bd6da78b526e8f1
sergio esteban torres <myplancash@gmail.com>
2024-03-27 11:24:01 -0500

    using the built-in fs module's promise-based API, which doesn't require you to import fs explicitly

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,18 +1,18 @@
-import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
+import { promises as fs } from 'fs'
 import { BlogPost } from '@/types'
 
 const postsDirectory = path.join(process.cwd(), 'blogposts')
 
 export async function getSortedPostsData() {
-    const fileNames = fs.readdirSync(postsDirectory);
+    const fileNames = await fs.readdir(postsDirectory);
     const allPostsData = await Promise.all(fileNames.map(async (fileName) => {
         const id = fileName.replace(/\.md$/, '');
         const fullPath = path.join(postsDirectory, fileName);
-        const fileContents = fs.readFileSync(fullPath, 'utf8');
+        const fileContents = await fs.readFile(fullPath, 'utf8');
         const matterResult = matter(fileContents);
         
         // Fetch contentHtml asynchronously
@@ -36,7 +36,7 @@ export async function getSortedPostsData() {
 
 export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const fileContents = await fs.readFile(fullPath, 'utf8');
 
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
